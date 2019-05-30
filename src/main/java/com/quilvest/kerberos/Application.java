3e657f588ac999f9068ac2460c7425b94da40a5f
alejandrogarciaperez <alejandro.garcia@flowable.com>
2019-05-30 12:29:39 +0200

    Initial commit

---
+++ src/main/java/com/quilvest/kerberos/Application.java
@@ -0,0 +1,22 @@
+package com.quilvest.kerberos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
